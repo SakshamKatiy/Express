18d1c6ea1e4560e4fed6800e7fb3ee728ae6d257
Saksham Katiyar <115263557+SakshamKatiy@users.noreply.github.com>
2024-01-29 23:58:03 +0530

    template engine hbs

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 8000;
 
 // used absolute path
 // console.log(path.join(__dirname, "../public"));
 const staticPath = path.join(__dirname, "../public/css");
+
+// to set the view engine
+app.set("view engine", "hbs");
+
 // Built in middleware
 app.use(express.static(staticPath));
 // Without middleware
+
+
+// template engine route
+app.get("/",(res,req)=>{
+    res.render("index",{
+        myName:"saksham",
+    });
+});
 app.get('/',(req, res) =>{
     res.status(200).send("working");
 });
